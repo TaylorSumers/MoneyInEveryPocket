fe313faf8401109be810b45f74ea75e29e899766
ZzONEx <mnakosergej@gmail.com>
2023-03-20 13:30:41 +0300

    Account info page added

--- Pages/AccountInfoPage.xaml.cs
+++ Pages/AccountInfoPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using MoneyInEveryPocket.Pages;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,18 +12,18 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MoneyInEveryPocket.Pages;
 
-namespace MoneyInEveryPocket
+namespace MoneyInEveryPocket.Pages
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для AccountInfoPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AccountInfoPage : Page
     {
-        public MainWindow()
+        public AccountInfoPage()
         {
             InitializeComponent();
-            MainFrame.Content = new AccountInfoPage();
         }
     }
 }
